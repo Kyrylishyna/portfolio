cda68ecb705cfa552de7dd9ae0afd8fe8b8f6077
Kyrylishyna <vkyryli1@stu.vistula.edu.pl>
2025-11-20 14:03:43 +0100

    New section "Contact Me" and view for a different device. Script for sending the info from form to google Sheet

--- JS/script.js
+++ JS/script.js
@@ -55,3 +55,30 @@ seeSoftBtn.addEventListener("click", () =>{
 })
 
 seeSoftBtn.style.display = "none";
+
+
+const sideMenu = document.getElementById("side_menu");
+
+function openMenu(){
+    sideMenu.style.right = "0";
+}
+
+function closeMenu(){
+    sideMenu.style.right = "-200px";
+}
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxhrSFwjmNLVlHg0t89IjoyKdMJ_etsYILKrrY_rfMr4vI5KShyzRkoOPLv9mgLbrrz/exec'
+  const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById("msg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML= ""
+        }, 5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
